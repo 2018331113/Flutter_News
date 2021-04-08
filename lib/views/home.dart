c9f602961f50f6bda18fc66d9a4a6aa8a31d9701
2018331113 <muhitmahmud404@gmail.com>
2021-04-09 02:01:39 +0600

    First commit

---
+++ lib/views/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListView.builder(
+          itemCount: ,
+          itemBuilder:(context,index){
+            return Container();
+          } 
+        ),
+      ],
+    );
+  }
+}
+
+class CategoryWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
