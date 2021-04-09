68e82f18be3a949401b465607732535c204325f2
2018331113 <muhitmahmud404@gmail.com>
2021-04-10 00:07:24 +0600

    made the home class from stless>>stfull

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,23 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news/helper/data.dart';
+import 'package:flutter_news/models/catergory_models.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<CategoryModel> categories = [];
+  @override
+  void initState() {
+    super.initState();
+    categories = getCategories();
+  }
+
+// always wrap listView items in a container if you want the direction to horizontal.
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ListView.builder(
-            itemCount: 1,
+        Container(
+          height: 70,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categories.length,
+            shrinkWrap: true,
             itemBuilder: (context, index) {
-              return Container();
-            }),
+              return CategoryWidget(
+                categoryName: categories[index].categoryName,
+                imgUrl: categories[index].imgURL,
+              );
+            },
+          ),
+        ),
       ],
     );
   }
 }
 
 class CategoryWidget extends StatelessWidget {
+  final String categoryName;
+  final String imgUrl;
+
+  CategoryWidget({this.categoryName, this.imgUrl});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Image.network(
+          imgUrl,
+          width: 120,
+          height: 60,
+        ),
+      ],
+    );
   }
 }
