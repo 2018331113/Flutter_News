bd4fd20afe62e0af1254c93e6a521645a826eb87
2018331113 <muhitmahmud404@gmail.com>
2021-04-10 00:07:43 +0600

    made a constant class

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+String kBusinessImgUrl =
+    "https://images.unsplash.com/photo-1444653614773-995cb1ef9efa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80";
+String kEntertainmentImgUrl =
+    "https://images.unsplash.com/photo-1533923156502-be31530547c4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80";
+String kGeneralImgUrl =
+    "https://images.unsplash.com/photo-1548222744-3db85aa5b0f9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+String kHealthImgUrl =
+    "https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+String kTechnologyImgUrl =
+    "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
