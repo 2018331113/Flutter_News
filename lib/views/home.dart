9ba2ae1bf355f1f7f44c9c5c54177d3bd5db14ab
2018331113 <muhitmahmud404@gmail.com>
2021-04-14 00:19:40 +0600

    homescreen now shows articles

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news/helper/data.dart';
+import 'package:flutter_news/helper/news.dart';
+import 'package:flutter_news/models/article_models.dart';
 import 'package:flutter_news/models/catergory_models.dart';
+import 'package:flutter_news/tiles/blog_tiles.dart';
+import 'package:flutter_news/tiles/category_tiles.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,52 +13,72 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<CategoryModel> categories = [];
-  @override
+  List<ArticleModel> articles = [];
+  bool _loading = true;
+
   void initState() {
     super.initState();
     categories = getCategories();
+    getNews();
+  }
+
+  getNews() async {
+    News newsClass = News();
+    await newsClass.getNews();
+    articles = newsClass.news;
+    setState(() {
+      _loading = false;
+    });
   }
 
 // always wrap listView items in a container if you want the direction to horizontal.
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 70,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: categories.length,
-            shrinkWrap: true,
-            itemBuilder: (context, index) {
-              return CategoryWidget(
-                categoryName: categories[index].categoryName,
-                imgUrl: categories[index].imgURL,
-              );
-            },
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class CategoryWidget extends StatelessWidget {
-  final String categoryName;
-  final String imgUrl;
-
-  CategoryWidget({this.categoryName, this.imgUrl});
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.network(
-          imgUrl,
-          width: 120,
-          height: 60,
-        ),
-      ],
-    );
+    return _loading
+        ? Container(
+            alignment: Alignment.center,
+            child: CircularProgressIndicator(),
+          )
+        : SingleChildScrollView(
+            physics: ScrollPhysics(),
+            child: Container(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  //Category
+                  Container(
+                    height: 120,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: categories.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return CategoryTile(
+                          categoryName: categories[index].categoryName,
+                          imgUrl: categories[index].imgURL,
+                        );
+                      },
+                    ),
+                  ),
+                  //Blogs
+                  Container(
+                    child: ListView.builder(
+                      physics:
+                          NeverScrollableScrollPhysics(), // must use this either won't scroll
+                      itemCount: articles.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return BlogTile(
+                          blogTitle: articles[index].title,
+                          blogDescribtion: articles[index].description,
+                          imgUrl: articles[index].urlToImage,
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
   }
 }
