1580820d56f9960c72c9e594c0a4e48238dbdda6
2018331113 <muhitmahmud404@gmail.com>
2021-04-15 17:03:59 +0600

    updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,5 +8,8 @@ String kHealthImgUrl =
     "https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
 String kTechnologyImgUrl =
     "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
-
+String kScienceImgUrl =
+    "https://images.unsplash.com/photo-1507668077129-56e32842fceb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80";
+String kSportsImgUrl =
+    "https://images.unsplash.com/photo-1541252260730-0412e8e2108e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=322&q=80";
 String kApiKey = "d0b24161afa942d2ac987edddaad4494";
