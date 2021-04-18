fbf9c8572e297e978949ac79764f1106163f7cdd
2018331113 <muhitmahmud404@gmail.com>
2021-04-18 16:40:09 +0600

    gave a progress indicator  and customAppbarr

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_news/widgets/customAppBar.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleView extends StatefulWidget {
@@ -14,35 +15,36 @@ class ArticleView extends StatefulWidget {
 class _ArticleViewState extends State<ArticleView> {
   final Completer<WebViewController> _completer =
       Completer<WebViewController>();
+  bool _loading = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0.0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Flutter"),
-            Text(
-              "News",
-              style: TextStyle(
-                color: Colors.blueAccent,
-              ),
+      appBar: CustomWidget().getAppBar(homePage: false),
+      body: Stack(
+        children: [
+          Container(
+            child: WebView(
+              initialUrl: widget.blogURL,
+              onPageFinished: (String finish) {
+                setState(() {
+                  _loading = false;
+                });
+              },
+              onWebViewCreated: ((WebViewController webViewController) {
+                _completer.complete(webViewController);
+              }),
             ),
-            SizedBox(
-              width: 55,
-            )
-          ],
-        ),
-      ),
-      body: Container(
-        child: WebView(
-          initialUrl: widget.blogURL,
-          onWebViewCreated: ((WebViewController webViewController) {
-            _completer.complete(webViewController);
-          }),
-        ),
+          ),
+          _loading
+              ? Container(
+                  alignment: Alignment.center,
+                  child: CircularProgressIndicator(),
+                )
+              : SizedBox(
+                  height: 0,
+                ),
+        ],
       ),
     );
   }
